8bb35415660851dce334b0d5923fa2610d4a32b9
Luka <dl200619d@student.etf.bg.ac.rs>
2024-07-13 17:19:00 +0200

    initial commit

---
+++ kviz/src/main/java/kviz/etf/bg/ac/rs/KvizApplication.java
@@ -0,0 +1,13 @@
+package kviz.etf.bg.ac.rs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvizApplication.class, args);
+	}
+
+}
---
+++ kviz/src/test/java/kviz/etf/bg/ac/rs/KvizApplicationTests.java
@@ -0,0 +1,13 @@
+package kviz.etf.bg.ac.rs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
